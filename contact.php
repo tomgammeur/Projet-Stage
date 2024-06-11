2b8e0017d3d90d085a89c87334ee349fc98fe7fd
Tom Gammeur <101002853+tomgammeur@users.noreply.github.com>
2024-06-11 13:37:42 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+$business = [
+    'name' => 'Laracasts',
+    'cost' => 15,
+    'categories' => ["Testing","PHP","Javascript", "HTML"]
+];
+
+
+function register($user){
+//
+//
+//
+//
+}
+
+require "contact.view.php";
