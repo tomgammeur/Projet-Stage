fd7c5ec018114b7b111fea22cfe9943b7716d6cd
Tom Gammeur <101002853+tomgammeur@users.noreply.github.com>
2024-06-11 14:14:54 +0200

    Add files via upload

---
+++ about.view.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+<div class="min-h-full">
+  <?php require('nav.php') ?>
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <p>Vous etes bien sur la page About</p>
+    </div>
+  </main>
+</div>
+
+</body>
+</html>
