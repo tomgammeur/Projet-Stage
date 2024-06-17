50c89bb1b15b1f1a08de97a2567f613f4af569cb
Tom Gammeur <101002853+tomgammeur@users.noreply.github.com>
2024-06-17 10:13:34 +0200

    Add files via upload

---
+++ tableau.php
@@ -0,0 +1,18 @@
+<?php
+
+$livres = ["livre1", "livre2", "livre3"];
+foreach ($livres as $title){
+    echo $title;
+}
+
+$ages = [17 , 34 , 49 , 2 , 5 , 18];
+$resultat = 0;
+foreach ($ages as $age) {
+    echo $age . " ";
+    $resultat = $resultat + $age;
+}
+
+echo $resultat / count($ages);
+
+echo "<br>";
+echo "test";
